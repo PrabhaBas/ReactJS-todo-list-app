cafebaeed0726b95a31170347107f3f4251ce76f
Prabhavathi B <prabhavathibaskar088@gmail.com>
2021-07-18 22:39:44 +0530

    Animation added

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ListItems.css';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
+import FlipMove from 'react-flip-move';
 function ListItems(props){
     const items = props.items;
     const listItems = items.map(item => {
@@ -22,8 +22,11 @@ function ListItems(props){
             
         </div>
     })
-    return(
-       <div>{listItems}</div>
-    )
+    return(<div>
+    <FlipMove duration={300} easing="ease-in-out">
+    {listItems}
+    </FlipMove>
+
+</div>);
 }
 export default ListItems;
